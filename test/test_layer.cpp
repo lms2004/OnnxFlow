ebdac27e093b31350c2ac35ea7bd0a394f38d823
llms26 <mxylms1210@163.com>
2025-06-09 00:31:56 +0800

    feat: 完成 MLA Pytorch 实现

--- test/test_layer.cpp
+++ test/test_layer.cpp
@@ -316,8 +316,3 @@ TEST_F(LayerParamTest, QuantizedCudaConversion) {
 
 }  // namespace
 }  // namespace mybase
-
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
